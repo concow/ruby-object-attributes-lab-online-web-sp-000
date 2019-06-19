f18113a7050ecadba513ffec6c817a9e06ac0d78
Connor Cowan <ccowan003@comcast.net>
2019-06-19 22:47:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,8 +4,8 @@ class Dog
     @name         #Without a getter method, you can not retrieve a value of an instance variable outside the class
   end
 
-  def name=(dog_name)     ##setter method that sets a value of an instance variable.
-    @name = dog_name
+  def name=(dog_name)     #setter method that sets a value of an instance variable.
+    @name = dog_name      #By using name=, you can now assign a new value outside the class
   end
 
   def breed
